ffb1619bacdeb3d05848ef6c03f635d49d63d84b
Doniagh <doniagharbi141@gmail.com>
2025-07-17 18:21:57 +0100

    db mongo

--- mongo-init/init-mongo.js
+++ mongo-init/init-mongo.js
@@ -15,63 +15,75 @@ db.createUser({
 
 // Create collections with some initial data
 db.createCollection('books');
-db.createCollection('authors');
-db.createCollection('categories');
+db.createCollection('loans');
 
 // Insert sample data
+db.loans.insertMany([
+  {
+     bookId: ObjectId("685bdb1f7509c0d090db7c6c"),
+     userEmail: "doniagharbi141@gmail.com",
+     loanDate: ISODate("2025-06-25T23:00:00.000Z"),
+     dueDate: ISODate("2025-07-09T23:00:00.000Z"),
+     returned: true,
+     _class: "com.library.library.Loan.Loan"
+   },
+   {
+     bookId: ObjectId("685bdb1f7509c0d090db7c6c"),
+     userEmail: "doniagharbi141@gmail.com",
+     loanDate: ISODate("2025-07-15T00:00:00.000Z"),
+     dueDate: ISODate("2025-07-29T00:00:00.000Z"),
+     returned: false,
+     _class: "com.library.library.Loan.Loan"
+   },
+   {
+     bookId: ObjectId("685bdb1f7509c0d090db7c6c"),
+     userEmail: "doniagharbi141@gmail.com",
+     loanDate: ISODate("2025-07-14T23:00:00.000Z"),
+     dueDate: ISODate("2025-07-28T23:00:00.000Z"),
+     returned: false,
+     _class: "com.library.library.Loan.Loan"
+   }
+]);
+
 db.books.insertMany([
   {
-    title: "Spring Boot in Action",
-    author: "Craig Walls",
-    isbn: "978-1617292545",
-    category: "Programming",
-    publishedDate: new Date("2015-12-06"),
+    _id : ObjectId("685aaacc5d0237087fd88e1f"),
+    title: "Story of little Frog",
+    quantity: 9,
+    author: "Antoine de Saint-Exupéry",
+    category: "Children",
     available: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
+    _class:  "com.library.library.book.Book"
   },
   {
-    title: "Microservices Patterns",
-    author: "Chris Richardson",
-    isbn: "978-1617294549",
-    category: "Architecture",
-    publishedDate: new Date("2018-10-30"),
-    available: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-]);
+    _id : ObjectId("685bdb1f7509c0d090db76c6"),
+    title: "Story of little Frog",
+    quantity: 0,
+    author: "Antoine de Saint-Exupéry",
+    category: "Children",
+    available: false,
+    _class:  "com.library.library.book.Book"
+  },
+  {
+      _id : ObjectId("685bdb337509c0d090db76c7"),
+      title: "Math Magic",
+      quantity: 0,
+      author: "Antoine de Saint-Exupéry",
+      category: "Children",
+      available: false,
+      _class:  "com.library.library.book.Book"
+    },
+    {
+          _id : ObjectId("685bdb417509c0d090db76c8"),
+          title: "Care Your Trees",
+          quantity: 2,
+          author: "Antoine de Saint-Exupéry",
+          category: "Children",
+          available: true,
+          _class:  "com.library.library.book.Book"
+        }
+])
 
-db.authors.insertMany([
-  {
-    name: "Craig Walls",
-    bio: "Software developer and author",
-    birthDate: new Date("1970-01-01"),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    name: "Chris Richardson",
-    bio: "Microservices expert and consultant",
-    birthDate: new Date("1965-01-01"),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-]);
 
-db.categories.insertMany([
-  {
-    name: "Programming",
-    description: "Programming and software development books",
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    name: "Architecture",
-    description: "Software architecture and design books",
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-]);
 
 print("Database initialized successfully!");
